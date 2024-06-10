03c73521e48df6fa732730f3c3e688a6c9948d7c
profe7 <89756053+profe7@users.noreply.github.com>
2024-06-10 22:28:58 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Tunggu database postgres siap..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
-
-    echo "Database siap"
-fi
-
+# perform database migration
 python manage.py makemigrations
 python manage.py migrate
 
+# start server
 exec "$@"
