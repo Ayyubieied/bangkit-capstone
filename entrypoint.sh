e1407f5db9711e7180154d6126b481a10c61ec26
profe7 <89756053+profe7@users.noreply.github.com>
2024-06-10 22:43:01 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# wait for the database to be available
+while ! nc -z db 5432; do
+  sleep 0.1
+done
+
 # perform database migration
 python manage.py makemigrations
 python manage.py migrate
